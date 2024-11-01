08ab929794856f8fca9f56db4a5eaa7a4f5c77d3
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-11-01 11:12:34 -0300

    Adiciona estrutura que armazena chave e url gerada pelo site exchange-api

---
+++ moeda.js
@@ -0,0 +1,5 @@
+// Armazena chave e url que foi gerada no site https://app.exchangerate-api.com/
+const apiKey = 'c231f1314a9e111663285fc7';
+const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+// Função para buscar taxa de câmbio da API

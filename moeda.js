49f993c04f3fd80d460536517462f750fb08704f
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-11-01 11:38:30 -0300

    Adiciona nova estrutura que traz o link de API e retorno do valor da moeda

--- moeda.js
+++ moeda.js
@@ -3,3 +3,21 @@ const apiKey = 'c231f1314a9e111663285fc7';
 const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
 
 // Função para buscar taxa de câmbio da API
+async function getExchangeRate(daMoeda, paraMoeda){
+  // Trazendo link da API
+    try{
+      const response = await fetch(`${apiUrl}${daMoeda}`);
+      const data = await responde.json();
+      // Retorno do valor da moeda
+      if(data.result === 'success'){
+        return data.conversion_rates[paraMoeda]; 
+
+      }else{
+        throw new Error ('Erro ao buscar a taxa de câmbio');
+      }
+
+    }catch(error){
+      console.error("Error: ", error);
+      return null;
+    }
+}

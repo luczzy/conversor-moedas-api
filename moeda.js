cd7158e54abd683305071d91209c51962356558c
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-11-08 10:50:36 -0300

    Adiciona estrutura de js com escutador para o botão converter

--- moeda.js
+++ moeda.js
@@ -1,23 +1,39 @@
-// Armazena chave e url que foi gerada no site https://app.exchangerate-api.com/
-const apiKey = 'c231f1314a9e111663285fc7';
-const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+const apiKey = 'e525988e1697fd6f84538bee'; // chave da API
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`; // válido por um mês
 
-// Função para buscar taxa de câmbio da API
-async function getExchangeRate(daMoeda, paraMoeda){
-  // Trazendo link da API
-    try{
-      const response = await fetch(`${apiUrl}${daMoeda}`);
-      const data = await responde.json();
-      // Retorno do valor da moeda
-      if(data.result === 'success'){
-        return data.conversion_rates[paraMoeda]; 
+// Função para buscar a taxa de câmbio da API
+async function getExchangeRate(moeda1, moeda2) {
+  try {
+    const response = await fetch(`${apiURL}${moeda1}`);
+    const data = await response.json();
 
-      }else{
-        throw new Error ('Erro ao buscar a taxa de câmbio');
-      }
-
-    }catch(error){
-      console.error("Error: ", error);
-      return null;
+    if (data.result === 'success') {
+      return data.conversion_rates[moeda2];
+    } else {
+      throw new Error('Erro ao buscar a taxa de câmbio');
     }
+  } catch (error) {
+    console.error("Error: ", error);
+    return null;
+  }
 }
+
+// Escutador para o botão "Converter"
+document.getElementById('currency-form').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const valor = parseFloat(document.getElementById('valor').value);
+  const moeda1 = document.getElementById('moeda1').value;
+  const moeda2 = document.getElementById('moeda2').value;
+
+  const ExchangeRate = await getExchangeRate(moeda1, moeda2);
+
+  if (ExchangeRate) {
+    const convertedValue = valor * ExchangeRate;
+    const conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${moeda2}`;
+  } else {
+    alert('Erro ao buscar a cotação. Tente novamente');
+  }
+});
+
